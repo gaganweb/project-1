8f7f14cc0350f8be26d0bc6731b106cc2b85687c
Gagan Dutta <gagan.d@bondinterior.local>
2023-10-20 11:42:16 +0400

    corrected errors in main.js file under pages folder

--- pages/js/main.js
+++ pages/js/main.js
@@ -1,15 +1,31 @@
-// main.js
+import { useEffect } from 'react';
 
-document.addEventListener('DOMContentLoaded', function () {
-  const openButton = document.querySelector('.nav-button');
-  const closeButton = document.querySelector('.close-button');
-  const navigation = document.querySelector('#navigation');
+function YourComponent() {
+  useEffect(() => {
+    // This code will only run on the client side
+    const openButton = document.querySelector('.nav-button');
+    const closeButton = document.querySelector('.close-button');
+    const navigation = document.querySelector('#navigation');
 
-  openButton.addEventListener('click', () => {
-    navigation.style.display = 'block';
-  });
+    openButton.addEventListener('click', () => {
+      navigation.style.display = 'block';
+    });
 
-  closeButton.addEventListener('click', () => {
-    navigation.style.display = 'none';
-  });
-});
+    closeButton.addEventListener('click', () => {
+      navigation.style.display = 'none';
+    });
+
+    return () => {
+      // Clean up event listeners if needed
+      openButton.removeEventListener('click', () => {
+        navigation.style.display = 'block';
+      });
+
+      closeButton.removeEventListener('click', () => {
+        navigation.style.display = 'none';
+      });
+    };
+  }, []); // The empty dependency array ensures this runs once after the component mounts
+
+  // Rest of your component code
+}
